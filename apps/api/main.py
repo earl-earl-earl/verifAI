64aa2a722a384e29fa54f01baef00549fc455936
= <ejboy0425@gmail.com>
2026-07-11 22:29:21 +0800

    chore: add initial API setup

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+  name: str
+  price: float
+  is_offer: bool = False
+
+items: list[Item] = []
 
 @app.get("/")
 def welcome():
   return {"response": "Hello World!"}
 
-@app.get("/index")
-def something():
-  return {"item": "cake"}
+@app.post("/items/")
+def add_item(item: Item):
+    items.append(item)
+    return {
+      "status": "Success",
+      "code": 200,
+      "message": f"{item.name} has been added successfully"
+    }
+
+@app.get("/items/")
+def get_all_items():
+  return items
